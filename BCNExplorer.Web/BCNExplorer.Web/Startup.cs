0459eeafc76d5392772af507f602bd479b812981
avolkov <avolkov.dev@gmail.com>
2016-08-18 16:56:14 +0300

    Initital commit

---
+++ BCNExplorer.Web/BCNExplorer.Web/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BCNExplorer.Web.Startup))]
+namespace BCNExplorer.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
