5c74e32a6a0fe7236ad3a8e37a2ba7951208e0aa
Andrey Volkov <netsky@bk.ru>
2016-08-29 22:38:15 +0300

    Address transaction list

--- BCNExplorer.Web/Models/AddressViewModel.cs
+++ BCNExplorer.Web/Models/AddressViewModel.cs
@@ -9,19 +9,22 @@ namespace BCNExplorer.Web.Models
     public class AddressViewModel
     {
         public string AddressId { get; set; }
-        public IEnumerable<string> TransactionIds { get; set; }
         public string UncoloredAddress { get; set; }
         public string ColoredAddress { get; set; }
         public double Balance { get; set; }
+        public int TotalTransactions { get; set; }
+        public TransactionIdList TransactionIdList { get; set; }
+        private const int PageSize = 20;
 
         public static AddressViewModel Create(NinjaAddress ninjaAddress)
         {
             return new AddressViewModel
             {
                 AddressId = ninjaAddress.AddressId,
-                TransactionIds = ninjaAddress.TransactionIds,
+                TransactionIdList = new TransactionIdList(ninjaAddress.TransactionIds, PageSize),
                 UncoloredAddress = ninjaAddress.UncoloredAddress,
-                ColoredAddress = ninjaAddress.ColoredAddress
+                ColoredAddress = ninjaAddress.ColoredAddress,
+                TotalTransactions = ninjaAddress.TransactionIds.Count()
             };
         }
     }
