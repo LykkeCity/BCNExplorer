87ed6c87b67326aa7a0a3acef0c3181377875652
avolkov <avolkov.dev@gmail.com>
2016-08-31 14:19:19 +0300

    Revert "New  error 404 handling"
    This reverts commit c16d3361fcfe4c0c9240054375bf4c430ba6593e.

--- BCNExplorer.Web/Controllers/ErrorController.cs
+++ BCNExplorer.Web/Controllers/ErrorController.cs
@@ -1,10 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace BCNExplorer.Web.Controllers
 {
     public class ErrorController:Controller
     {
-        [Route("notfound")]
         public ActionResult NotFound()
         {
             return View();
