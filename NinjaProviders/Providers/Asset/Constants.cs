4f7f5a10e9d6341057d0fb808a63c12e2722871f
Andrey Volkov <netsky@bk.ru>
2016-09-06 15:31:01 +0300

    New Asset list

--- NinjaProviders/Providers/Asset/Constants.cs
+++ NinjaProviders/Providers/Asset/Constants.cs
@@ -6,13 +6,18 @@ namespace Providers.Providers.Asset
     {
         public static IEnumerable<string> AssetDefinitonUrls => new[]
         {
-            "https://www.lykkex.com/lkeUSD",
-            "https://www.lykkex.com/lkeEUR",
-            "https://www.lykkex.com/lkeCHF",
+            "https://lykke.com/asset/lkeUSD",
+            "https://lykke.com/asset/lkeEUR",
+            "https://lykke.com/asset/lkeCHF",
+            "https://lykke.com/asset/lkeGBP",
+            "https://lykke.com/asset/lkeJPY",
             "https://lykke.com/asset/LKK",
             "https://www.lykkex.com/LykkeCorpEquity",
-            "https://www.lykkex.com/lkeJPY",
-            "https://www.lykkex.com/lkeGBP"
+            "https://www.lykkex.com/lkeEUR",
+            "https://www.lykkex.com/lkeUSD",
+            "https://www.lykkex.com/lkeCHF",
+            "https://www.lykkex.com/lkeGBP",
+            "https://www.lykkex.com/lkeJPY"
         };
     }
 }
