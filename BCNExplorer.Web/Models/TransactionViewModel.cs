22fb6d8e17cf5811e05d9e07963df5d3d2570860
avolkov <avolkov.dev@gmail.com>
2016-09-27 15:48:35 +0300

    Transaction Details Refactoring

--- BCNExplorer.Web/Models/TransactionViewModel.cs
+++ BCNExplorer.Web/Models/TransactionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Common;
 using Providers.TransportTypes;
 using Providers.TransportTypes.Asset;
 using Providers.TransportTypes.Ninja;
@@ -12,13 +13,13 @@ namespace BCNExplorer.Web.Models
         public string TransactionId { get; set; }
         public bool IsColor { get; set; }
         public bool IsCoinBase { get; set; }
-
-        public double Fees { get; set; }
         public int AssetsCount { get; set; }
         public bool IsConfirmed { get; set; }
         public BlockViewModel Block { get; set; }
-        public IEnumerable<InOutsByAsset> InOutsByAssets { get; set; } 
-        public AssetDictionary AssetDic { get; set; }
+        
+        public BitcoinAsset Bitcoin { get; set; }
+
+        public IEnumerable<ColoredAsset> ColoredAssets { get; set; } 
 
         #region Classes 
 
@@ -48,66 +49,152 @@ namespace BCNExplorer.Web.Models
             }
         }
 
-        public class InOut
+        public class BitcoinAsset
         {
-            public string TransactionId { get; set; }
-            public string Address { get; set; }
-            public int Index { get; set; }
-            public double Value { get; set; }
-            public string AssetId { get; set; }
-            public double Quantity { get; set; }
+            public bool IsCoinBase { get; set; }
+            public IEnumerable<In> Ins { get; set; }
+            public IEnumerable<Out> Outs { get; set; }
+            public decimal Fees { get; set; }
 
-            public static IEnumerable<InOut> Create(IEnumerable<NinjaTransaction.InOut> source)
+            public static BitcoinAsset Create(double fees,
+                bool isCoinBase,
+                IEnumerable<NinjaTransaction.InOut> ins,
+                IEnumerable<NinjaTransaction.InOut> outs)
             {
-                return source.Select(p => new InOut
+                return new BitcoinAsset
                 {
-                    Address = p.Address,
-                    AssetId = p.AssetId,
-                    Index = p.Index,
-                    Quantity = p.Quantity,
-                    TransactionId = p.TransactionId,
-                    Value = p.Value
-                });
-            } 
+                    Fees = BitcoinUtils.SatoshiToBtc(fees),
+                    IsCoinBase = isCoinBase,
+                    Ins = In.Create(ins),
+                    Outs = Out.Create(outs)
+                };
+            }
+
+            public class In
+            {
+                public decimal Value { get; set; }
+                public string Address { get; set; }
+                public string PreviousTransactionId { get; set; }
+                public bool IsUnrecoginzedAddress => string.IsNullOrEmpty(Address);
+                public static IEnumerable<In> Create(IEnumerable<NinjaTransaction.InOut> ins)
+                {
+                    return ins.Where(p => p.Value != 0).Select(p => new In
+                    {
+                        Value = BitcoinUtils.SatoshiToBtc(p.Value * (-1)), 
+                        Address = p.Address,
+                        PreviousTransactionId = p.TransactionId
+                    });
+                }
+            }
+
+            public class Out
+            {
+                public decimal Value { get; set; }
+                public string Address { get; set; }
+                public bool IsUnrecoginzedAddress => string.IsNullOrEmpty(Address);
+                public static IEnumerable<Out> Create(IEnumerable<NinjaTransaction.InOut> outs)
+                {
+                    return outs.Where(p => p.Value != 0).Select(p => new Out
+                    {
+                        Value = BitcoinUtils.SatoshiToBtc(p.Value),
+                        Address = p.Address
+                    });
+                }
+            }
+        }
+
+        public class ColoredAsset
+        {
+            public string AssetId { get; set; }
+            public int Divisibility { get; set; }
+            public string ShortName { get; set; }
+            public string Name { get; set; }
+            public string IconImageUrl { get; set; }
+
+            public IEnumerable<In> Ins { get; set; }
+            public IEnumerable<Out> Outs { get; set; }
+
+            #region Classes
+
+            public class In
+            {
+                public string PreviousTransactionId { get; set; }
+                public double Quantity { get; set; }
+                public string Address { get; set; }
+                public bool IsUnrecoginzedAddress => string.IsNullOrEmpty(Address);
+
+                public static In Create(NinjaTransaction.InOut sourceIn, int divisibility, IEnumerable<NinjaTransaction.InOut> outs )
+                {
+                    var l = outs.Select(itm => itm.Quantity - sourceIn.Quantity);
+                    var def = l.FirstOrDefault();
+
+                    return new In
+                    {
+                        Address = sourceIn.Address,
+                        PreviousTransactionId = sourceIn.TransactionId,
+                        Quantity = BitcoinUtils.CalculateColoredAssetQuantity((def != 0 ? def : sourceIn.Quantity), divisibility)
+                    };
+                }
+            }
+
+            public class Out
+            {
+                public double Quantity { get; set; }
+                public string Address { get; set; }
+                public bool IsUnrecoginzedAddress => string.IsNullOrEmpty(Address);
+
+                public static Out Create(NinjaTransaction.InOut sourceOut, int divisibility)
+                {
+                    return new Out
+                    {
+                        Address = sourceOut.Address,
+                        Quantity = BitcoinUtils.CalculateColoredAssetQuantity(sourceOut.Quantity, divisibility)
+                    };
+                }
+            }
+            
+            #endregion
+
+            public static ColoredAsset Create(NinjaTransaction.InOutsByAsset inOutsByAsset, AssetDictionary assetDictionary)
+            {
+                var divisibility = assetDictionary.GetAssetProp(inOutsByAsset.AssetId, p => p.Divisibility, 0);
+
+                var result = new ColoredAsset
+                {
+                    AssetId = inOutsByAsset.AssetId,
+                    Divisibility = divisibility,
+                    Name = assetDictionary.GetAssetProp(inOutsByAsset.AssetId, p => p.Name, null) ?? inOutsByAsset.AssetId,
+                    IconImageUrl = assetDictionary.GetAssetProp(inOutsByAsset.AssetId, p => p.IconUrl, null),
+                    ShortName = assetDictionary.GetAssetProp(inOutsByAsset.AssetId, p => p.NameShort, null),
+                    Ins = inOutsByAsset.TransactionIn.Select(p=> In.Create(p, divisibility, inOutsByAsset.TransactionsOut)),
+                    Outs = inOutsByAsset.TransactionsOut.Select(p => Out.Create(p, divisibility))
+                };
+
+                return result;
+            }
         }
 
         #endregion
 
         public static TransactionViewModel Create(NinjaTransaction ninjaTransaction, IDictionary<string, AssetDefinition> assetDictionary)
         {
+            var bc = ninjaTransaction.TransactionsByAssets.First(p => !p.IsColored);
+            var colored = ninjaTransaction.TransactionsByAssets.Where(p => p.IsColored).OrderBy(p => p.AssetId);
+            var assetDic = AssetDictionary.Create(assetDictionary);
+
             var result = new TransactionViewModel
             {
                 TransactionId = ninjaTransaction.TransactionId,
                 IsCoinBase = ninjaTransaction.IsCoinBase,
                 IsColor = ninjaTransaction.IsColor,
                 Block = BlockViewModel.Create(ninjaTransaction.Block),
-                Fees = ninjaTransaction.Fees,
-                InOutsByAssets = InOutsByAsset.Create(ninjaTransaction.TransactionsByAssets),
                 AssetsCount = ninjaTransaction.TransactionsByAssets.Count(p => p.IsColored),
                 IsConfirmed = ninjaTransaction.Block != null,
-                AssetDic = AssetDictionary.Create(assetDictionary)
+                Bitcoin = BitcoinAsset.Create(ninjaTransaction.Fees, ninjaTransaction.IsCoinBase, bc.TransactionIn, bc.TransactionsOut ),
+                ColoredAssets = colored.Select(p => ColoredAsset.Create(p, assetDic))
             };
             
             return result;
-        }
-
-        public class InOutsByAsset
-        {
-            public bool IsColored { get; set; }
-            public string AssetId { get; set; }
-            public IEnumerable<InOut> TransactionIn { get; set; }
-            public IEnumerable<InOut> TransactionsOut { get; set; }
-
-            public static IEnumerable<InOutsByAsset> Create(IEnumerable<NinjaTransaction.InOutsByAsset> source)
-            {
-                return source.Select(p => new InOutsByAsset
-                {
-                    AssetId = p.AssetId,
-                    IsColored = p.IsColored,
-                    TransactionIn = InOut.Create(p.TransactionIn),
-                    TransactionsOut = InOut.Create(p.TransactionsOut)}
-                );
-            }
         }
     }
 }
