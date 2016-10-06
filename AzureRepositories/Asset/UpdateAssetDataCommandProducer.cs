88cf600f5a747611ce9d1f14446519ea36d27741
avolkov <avolkov.dev@gmail.com>
2016-10-06 12:42:55 +0300

    Command producer

---
+++ AzureRepositories/Asset/UpdateAssetDataCommandProducer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureStorage.Queue;
+
+namespace AzureRepositories.Asset
+{
+    public class UpdateAssetDataCommandProducer
+    {
+        private readonly IQueueExt _queueExt;
+
+        public UpdateAssetDataCommandProducer(IQueueExt queueExt)
+        {
+            _queueExt = queueExt;
+        }
+
+        public Task CreateUpdateAssetDataCommand(string url)
+        {
+            return _queueExt.PutMessageAsync(url);
+        } 
+    }
+}
---
+++ AzureRepositories/JobsQueueNames.cs
@@ -0,0 +1,7 @@
+﻿namespace AzureRepositories
+{
+    public class JobsQueueNames
+    {
+        public const string AddNewAssetsQueueName = "newassets";
+    }
+}
