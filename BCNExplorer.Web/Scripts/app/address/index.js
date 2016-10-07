cf3607ab517f38b570b2f4f198b3c40424ec7fa0
avolkov <avolkov.dev@gmail.com>
2016-10-07 17:57:06 +0300

    js address highlight fix

--- BCNExplorer.Web/Scripts/app/address/index.js
+++ BCNExplorer.Web/Scripts/app/address/index.js
@@ -3,7 +3,7 @@
     var unColoredAddress = $('#uncolored-address').val();
     var currentAddress = $('#current-address').val();
     $('body').on('transactions-loaded', function() {
-        $('[data-address="' + coloredAddress + '"],[data-address="' + unColoredAddress + '"],[data-address="' + currentAddress + ']"')
+        $('[data-address="' + coloredAddress + '"],[data-address="' + unColoredAddress + '"],[data-address="' + currentAddress + '"]')
             .addClass('current-address-transaction');
     });
 })
