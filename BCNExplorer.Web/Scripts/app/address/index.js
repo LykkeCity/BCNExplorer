e7d90c834acdf3b1b546c827f3c35da13e3eebbe
avolkov <avolkov.dev@gmail.com>
2016-10-07 13:04:36 +0300

    highlight current transaction js fix

--- BCNExplorer.Web/Scripts/app/address/index.js
+++ BCNExplorer.Web/Scripts/app/address/index.js
@@ -1,8 +1,9 @@
 ﻿$(function () {
+    var coloredAddress = $('#colored-address').val();
+    var unColoredAddress = $('#uncolored-address').val();
     var currentAddress = $('#current-address').val();
-    var currentColoredAddress = $('#current-colored-address').val();
     $('body').on('transactions-loaded', function() {
-        $('[data-address=' + currentAddress + '],[data-address=' + currentColoredAddress + ']')
+        $('[data-address="' + coloredAddress + '"],[data-address="' + unColoredAddress + '"],[data-address="' + currentAddress + ']"')
             .addClass('current-address-transaction');
     });
 })
