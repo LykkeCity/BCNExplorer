70a963387795f538d4631c4bd113d4442920d8ea
avolkov <avolkov.dev@gmail.com>
2016-10-13 13:55:47 +0300

    Update nuget dependencies

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage.Auth;
-using NBitcoin;
-using NBitcoin.Indexer;
-using NBitcoin.OpenAsset;
+﻿using AzureRepositories;
+using AzureRepositories.Binders;
+using Common.IocContainer;
+using Common.Log;
+using Core.Settings;
+using JobsCommon;
+using Providers;
 
 namespace TestConsole
 {
@@ -14,19 +12,20 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            var settings = GeneralSettingsReader.ReadGeneralSettings<BaseSettings>(JobsConnectionStringSettings.ConnectionString);
+            var logToTable = new LogToConsole();
 
-            //var _client = new IndexerClient(configurationMainNet);
 
-            //var block =
-            //    _client.GetBlock(
-            //        uint256.Parse("0000000000000000011efc2e2f7f771370597a835f63dd945df2f5467223303b"));
 
-            //var txId = block.GetHash().AsBitcoinSerializable().Value.ToString();
-            //var tx = block.Transactions.FirstOrDefault(p=>p.GetHash()== uint256.Parse("0c0a21b640b1011ed397138d76b81375d35c10e419b52fc887677634dd7bcd4f"));
+        }
 
-            //var t2 = tx.GetColoredMarker().GetMetadataUrl();
-            //var url = t2;
+        private static void InitContainer(IoC container, BaseSettings settings, ILog log)
+        {
+            container.Register<ILog>(log);
 
+            container.BindProviders(settings, log);
+            container.Register(settings);
+            container.BindAzureRepositories(settings, log);
         }
     }
 }
