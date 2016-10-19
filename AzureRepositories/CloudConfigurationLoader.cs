0f52ee14aabfe33536ebcd81261d9c4c1c2e46fb
avolkov <avolkov.dev@gmail.com>
2016-10-19 17:36:48 +0300

    ping jobs 2

---
+++ AzureRepositories/CloudConfigurationLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Azure;
+
+namespace AzureRepositories
+{
+    public static class CloudConfigurationLoader
+    {
+        public static T ReadCloudConfiguration<T>() where T : new()
+        {
+            var properties = typeof(T).GetProperties();
+            T result = new T();
+
+            foreach (var property in properties)
+            {
+                if (property.GetSetMethod() != null)
+                {
+                    var type = property.PropertyType;
+                    property.SetValue(result, Convert.ChangeType(CloudConfigurationManager.GetSetting(property.Name), type));
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- PingJob/Program.cs
+++ PingJob/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using AzureRepositories;
+using Core.Settings;
+using JobsCommon;
+using Microsoft.Azure.WebJobs;
 
 namespace PingJob
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var appSettings = CloudConfigurationLoader.ReadCloudConfiguration<AppSettings>();
+            var settings = GeneralSettingsReader.ReadGeneralSettings<BaseSettings>(appSettings.ConnectionString);
+
+            var container = new DResolver();
+            InitContainer(container, settings, appSettings);
+
+            var config = new JobHostConfiguration
+            {
+                JobActivator = container,
+                StorageConnectionString = settings.Db.LogsConnString,
+                DashboardConnectionString = settings.Db.LogsConnString,
+                Tracing = { ConsoleLevel = TraceLevel.Error }
+            };
+            config.UseTimers();
+
+            if (settings.Jobs.IsDebug)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            var host = new JobHost(config);
+            host.RunAndBlock();
+        }
+
+        private static void InitContainer(DResolver container, BaseSettings settings, AppSettings appSettings)
+        {
+            container.IoC.Register(appSettings);
+            container.IoC.Register(settings);
+
+            container.IoC.RegisterSingleTone<PingFunctions>();
         }
     }
 }
