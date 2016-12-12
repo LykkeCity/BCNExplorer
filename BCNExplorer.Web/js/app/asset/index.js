7f5adcc01519a5093ee6d458cba6d45f288ecdd1
avolkov <avolkov.dev@gmail.com>
2016-12-12 18:47:52 +0300

    load coinholders via ajax

--- BCNExplorer.Web/js/app/asset/index.js
+++ BCNExplorer.Web/js/app/asset/index.js
@@ -1,3 +1,22 @@
 ﻿$(function() {
-    
+    var $initialLoadPanel = $('#js-load-panels').find('[data-load-url]:visible').first();
+    var $loader = $('#js-panel-loader');
+    $initialLoadPanel.load($initialLoadPanel.data('load-url'), function() {
+        $loader.hide();
+    });
+
+    $('body').on('click', '.js-load-panel-loader', function () {
+        var $self = $(this);
+        var url = $self.attr('href');
+        var $panel = $($self.data('load-container'));
+
+        $loader.show();
+        $panel.hide();
+        $panel.load(url, function () {
+            $loader.hide();
+            $panel.show();
+        });
+
+        return false;
+    });
 })
