43be8a97ec3e7329f7785e65c2564ad774042f69
avolkov <avolkov.dev@gmail.com>
2016-12-13 17:26:58 +0300

    Generate qr code on address page

---
+++ BCNExplorer.Web/HtmlHelpers/QRCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BCNExplorer.Web.HtmlHelpers
+{
+    public static class QRCodeHtmlHelper
+    {
+        public static MvcHtmlString QRCode(this HtmlHelper htmlHelper, string data, int size = 80, object htmlAttributes = null)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+            if (size < 1)
+                throw new ArgumentOutOfRangeException("size", size, "Must be greater than zero.");
+
+            var url = $"https://lykke-qr.azurewebsites.net/QR/{HttpUtility.UrlEncode(data)}.gif";
+
+            var tag = new TagBuilder("img");
+            if (htmlAttributes != null)
+                tag.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+            tag.Attributes.Add("src", url);
+            tag.Attributes.Add("width", size.ToString());
+            tag.Attributes.Add("height", size.ToString());
+
+            return new MvcHtmlString(tag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+
+    public enum QRCodeErrorCorrectionLevel
+    {
+        /// <summary>Recovers from up to 7% erroneous data.</summary>
+        Low,
+        /// <summary>Recovers from up to 15% erroneous data.</summary>
+        Medium,
+        /// <summary>Recovers from up to 25% erroneous data.</summary>
+        QuiteGood,
+        /// <summary>Recovers from up to 30% erroneous data.</summary>
+        High
+    }
+}
