4c8abe21836ffabf1348dc547ed5c70037b92852
avolkov <avolkov.dev@gmail.com>
2017-01-13 16:31:04 +0300

    Load balance ajax UPD

--- BCNExplorer.Web/js/app/address/index.js
+++ BCNExplorer.Web/js/app/address/index.js
@@ -8,13 +8,29 @@
         });
     })();
 
-    var coloredAddress = $('#colored-address').val();
-    var unColoredAddress = $('#uncolored-address').val();
-    var currentAddress = $('#current-address').val();
-    $('body').on('transactions-loaded', function() {
-        $('[data-address="' + coloredAddress + '"],[data-address="' + unColoredAddress + '"],[data-address="' + currentAddress + '"]')
-            .addClass('current-address-transaction');
-    });
+    (function () {
+        var $loadContainer = $('#js-balance-load-contaner');
+        var $loader = $('#js-balance-loader');
+        var loadUrl = $loadContainer.data('load-url');
 
+        var loadBalance = function() {
+            $.ajax(loadUrl).done(function (resp) {
+                $loadContainer.html(resp);
+                $loader.hide();
+            });
+        };
+
+        loadBalance();
+    })();
+
+    (function () {
+        var coloredAddress = $('#colored-address').val();
+        var unColoredAddress = $('#uncolored-address').val();
+        var currentAddress = $('#current-address').val();
+        $('body').on('transactions-loaded', function () {
+            $('[data-address="' + coloredAddress + '"],[data-address="' + unColoredAddress + '"],[data-address="' + currentAddress + '"]')
+                .addClass('current-address-transaction');
+        });
+    })();
 
 })
