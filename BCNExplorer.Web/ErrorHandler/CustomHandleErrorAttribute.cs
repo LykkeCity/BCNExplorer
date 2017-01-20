2961758f61e6c2fa6059f20f50ea27caba0c1acf
Andrey Volkov <netsky@bk.ru>
2017-01-21 00:43:16 +0300

    write UserAgent and Referrer on error

--- BCNExplorer.Web/ErrorHandler/CustomHandleErrorAttribute.cs
+++ BCNExplorer.Web/ErrorHandler/CustomHandleErrorAttribute.cs
@@ -1,9 +1,41 @@
-using System;using System.IO;using System.Text;using System.Threading.Tasks;using System.Web.Mvc;using Common;using Microsoft.ApplicationInsights;namespace BCNExplorer.Web.ErrorHandler{    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]    public class CustomHandleErrorAttribute : HandleErrorAttribute    {        public override void OnException(ExceptionContext filterContext)        {            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)            {
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Common;
+using Microsoft.ApplicationInsights;
+
+namespace BCNExplorer.Web.ErrorHandler
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class CustomHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
                 var ai = new TelemetryClient();
                 ai.TrackException(filterContext.Exception);
 
                 string controller = filterContext.RouteData.Values["controller"].ToString();
-                string action = filterContext.RouteData.Values["action"].ToString();                var request = filterContext.RequestContext.HttpContext.Request;                Task.Run(() => App.Log.WriteError(controller, action, new                {                    request.Url?.PathAndQuery,                    request.HttpMethod,                    Body = GetDocumentContents(request)                }.ToJson(), filterContext.Exception).ConfigureAwait(false)).Wait();                                base.OnException(filterContext);            }        }        private static string GetDocumentContents(System.Web.HttpRequestBase Request)
+                string action = filterContext.RouteData.Values["action"].ToString();
+                var request = filterContext.RequestContext.HttpContext.Request;
+
+                Task.Run(() => App.Log.WriteError(controller, action, new
+                {
+                    request.Url?.PathAndQuery,
+                    request.HttpMethod,
+                    Body = GetDocumentContents(request),
+                    request.UserAgent,
+                    request.UrlReferrer
+                }.ToJson(), filterContext.Exception).ConfigureAwait(false)).Wait();
+                
+                base.OnException(filterContext);
+            }
+        }
+
+        private static string GetDocumentContents(System.Web.HttpRequestBase Request)
         {
             string documentContents;
             using (Stream receiveStream = Request.InputStream)
@@ -14,4 +46,6 @@ using System;using System.IO;using System.Text;using System.Threading.Tasks;
                 }
             }
             return documentContents;
-        }    }}
+        }
+    }
+}
