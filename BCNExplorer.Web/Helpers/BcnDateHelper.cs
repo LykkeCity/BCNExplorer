b57417d638394c409244891a74845e6812d7c5a0
avolkov <avolkov.dev@gmail.com>
2017-01-20 13:49:29 +0300

    format date specific way if days past date more than 7 days - do not show day of week

---
+++ BCNExplorer.Web/Helpers/BcnDateHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BCNExplorer.Web.Helpers
+{
+    public static class BcnDateHelper
+    {
+        public static string ToStringBcnExplolerFormat(this DateTime date)
+        {
+            if (DateTime.Now - date > TimeSpan.FromDays(7))
+            {
+                return date.ToString("MMMM dd, yyyy h:mm tt");
+            }
+
+            return date.ToString("f");
+        }
+    }
+}
