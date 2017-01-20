4ac0b19e2d75a72ad6d09354a40644230196ee5c
avolkov <avolkov.dev@gmail.com>
2017-01-20 13:13:40 +0300

    Parse job: confirmation limit change(2->5)

--- AssetCoinHolders/TimerFunctions/ParseBlocksFunctions.cs
+++ AssetCoinHolders/TimerFunctions/ParseBlocksFunctions.cs
@@ -37,7 +37,7 @@ namespace AssetCoinHoldersScanner.TimerFunctions
 
                 var lastParsedBlockHeight = await _parsedBlockRepository.GetLastParsedBlockHeightAsync();
                 // to put notconfirmed tx-s (at last parse block iteration) in prev block. Now this tx have to be confirmed
-                var startParseBlock = lastParsedBlockHeight != 0 ? (lastParsedBlockHeight - 2) : 0;
+                var startParseBlock = lastParsedBlockHeight != 0 ? (lastParsedBlockHeight - 6) : 0;
 
                 for (var i = startParseBlock; i <= mainChain.Tip.Height; i++)
                 {
